197c3057d505906261bfb0da563ca66673cd0958
giulio <giulio09@marcov.it>
2021-02-04 16:08:05 +0000

    Fix icon manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
+        icon: `src/images/gatsby-icon.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
